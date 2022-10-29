cfa2bbbf8f193efc66432d982da91b3f5afecbbb
RezAdmin <chathura.prabhashwara@colombo.rezgateway.com>
2022-10-29 12:00:35 +0530

    initiating the react application

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        Hi My Name is{" "}
+        <a href="https://pulasthiaberathne.medium.com/the-git-7cc8dbd11352">
+          Pulasthi Aberathne
+        </a>
+      </h1>
+    </div>
+  );
+}
+
+export default App;
